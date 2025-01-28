775ed84131e32c7dad08003a6be2988b999b7ed8
Bartosz <bstefaniak@estrategysolutions.com>
2025-01-28 01:22:01 +0100

    feat: add profile get endpoint

--- internal/common/middlewares/check_auth_middleware.go
+++ internal/common/middlewares/check_auth_middleware.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -14,22 +13,19 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var (
-	ErrInvalidToken = errors.New("invalid token")
-)
-
 func CheckAuth(c *gin.Context) {
+
 	authHeader := c.GetHeader("Authorization")
 
 	if authHeader == "" {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
 
 	authToken := strings.Split(authHeader, " ")
-
 	if len(authToken) != 2 || authToken[0] != "Bearer" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": ErrInvalidToken.Error()})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
@@ -37,22 +33,23 @@ func CheckAuth(c *gin.Context) {
 	tokenString := authToken[1]
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
 
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": ErrInvalidToken.Error()})
+	fmt.Println("ITS TOKEN:: ", err)
+
+	if err != nil || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
-
 	if !ok {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": ErrInvalidToken.Error()})
-		c.AbortWithStatus(http.StatusUnauthorized)
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		c.Abort()
 		return
 	}
 
@@ -63,13 +60,17 @@ func CheckAuth(c *gin.Context) {
 	}
 
 	var user entities.User
+
 	initializers.DB.Where("id=?", claims["id"]).Find(&user)
 
 	if user.ID == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User id is empty"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
 
-	c.Set("currentUser", claims["user"])
+	c.Set("currentUser", user)
+
 	c.Next()
+
 }
--- internal/user/controller/user_controller.go
+++ internal/user/controller/user_controller.go
@@ -3,11 +3,25 @@ package controller
 import (
 	"net/http"
 
+	"github.com/bartholomeas/hwheels_api/config/initializers"
+	"github.com/bartholomeas/hwheels_api/internal/auth/entities"
 	"github.com/gin-gonic/gin"
 )
 
 func GetUserProfile(c *gin.Context) {
-	user, _ := c.Get("currentUser")
+	currentUser, _ := c.Get("currentUser")
+	user, ok := currentUser.(entities.User)
 
-	c.JSON(http.StatusOK, gin.H{"user": user})
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not get user from context"})
+		return
+	}
+
+	var userWithProfile entities.User
+	initializers.DB.
+		Preload("Profile").
+		Where("id = ?", user.ID).
+		First(&userWithProfile)
+
+	c.JSON(http.StatusOK, userWithProfile)
 }
